91dea71593dff0fada72c8c15325d6fb52e7fc19
Amer Madi <firas.madi@prinovausa.com>
2026-07-01 10:26:10 -0500

    Rebrand Astro starter to The HalfCourt (NBA blog + newsletter)

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'The HalfCourt';
+export const SITE_DESCRIPTION = 'NBA takes, analysis, and a newsletter for people who watch too much basketball.';
